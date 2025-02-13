491362aefa76495a18d48282bcbd3f3cc12aa991
robin <makenenjoy@gmail.com>
2025-02-13 12:19:37 +0100

    scala graph processing test

--- simulator/build.sbt
+++ simulator/build.sbt
@@ -1,7 +1,9 @@
-name := "Simple Project"
+name := "Simulator"
 
 version := "1.0"
 
 scalaVersion := "2.12.18"
 
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.5.4"
+
+libraryDependencies += "org.apache.spark" %% "spark-graphx" % "3.5.4"
---
+++ simulator/src/main/scala/SimulatorApp.scala
@@ -0,0 +1,34 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.graphx.{Graph, VertexId}
+import org.apache.spark.graphx.util.GraphGenerators
+
+object SimulatorApp {
+  def main(args: Array[String]): Unit = {
+    val logFile = "README.md" // Should be some file on your system
+    val spark = SparkSession.builder.appName("Simulator").getOrCreate()
+
+    // A graph with edge attributes containing distances
+    val graph: Graph[Long, Double] =
+      GraphGenerators.logNormalGraph(spark.sparkContext, numVertices = 100).mapEdges(e => e.attr.toDouble)
+    val sourceId: VertexId = 42 // The ultimate source
+    // Initialize the graph such that all vertices except the root have distance infinity.
+    val initialGraph = graph.mapVertices((id, _) =>
+        if (id == sourceId) 0.0 else Double.PositiveInfinity)
+    val sssp = initialGraph.pregel(Double.PositiveInfinity)(
+      (id, dist, newDist) => math.min(dist, newDist), // Vertex Program
+      triplet => {  // Send Message
+        if (triplet.srcAttr + triplet.attr < triplet.dstAttr) {
+          Iterator((triplet.dstId, triplet.srcAttr + triplet.attr))
+        } else {
+          Iterator.empty
+        }
+      },
+      (a, b) => math.min(a, b) // Merge Message
+    )
+    println(sssp.vertices.collect.mkString("\n"))
+
+
+    spark.stop()
+  }
+}
