31b16b738d448c9b46abebb3faa630a734520c67
robin <makenenjoy@gmail.com>
2025-02-13 16:30:28 +0100

    writing to a source

--- simulator/src/main/scala/SimulatorApp.scala
+++ simulator/src/main/scala/SimulatorApp.scala
@@ -3,9 +3,11 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.graphx.{Graph, VertexId}
 import org.apache.spark.graphx.util.GraphGenerators
 
+
 object SimulatorApp {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder.appName("Simulator").getOrCreate()
+    import spark.implicits._
 
     // A graph with edge attributes containing distances
     val graph: Graph[Long, Double] =
@@ -14,6 +16,9 @@ object SimulatorApp {
     // Initialize the graph such that all vertices except the root have distance infinity.
     val initialGraph = graph.mapVertices((id, _) =>
         if (id == sourceId) 0.0 else Double.PositiveInfinity)
+
+// graph.vertices.filter { case (id, (name, pos)) => pos == "postdoc" }.count
+
     val sssp = initialGraph.pregel(Double.PositiveInfinity)(
       (id, dist, newDist) => math.min(dist, newDist), // Vertex Program
       triplet => {  // Send Message
@@ -25,7 +30,12 @@ object SimulatorApp {
       },
       (a, b) => math.min(a, b) // Merge Message
     )
-    println(sssp.vertices.collect.mkString("\n"))
+    val df = sssp.vertices.map { case (id, dist) => dist }.toDF()
+    .write
+    .format("console")
+    .save()
+    println("asdkasjdlkjasldkj\n")
+    // println(sssp.vertices.collect.mkString("\n"))
 
     spark.stop()
   }
