3f602ca8949db662bd14b13db48011763d951a2b
Robin <makenenjoy@gmail.com>
2025-02-25 12:33:45 +0100

    trying to send kafka message not working

--- simulator/build.sbt
+++ simulator/build.sbt
@@ -7,3 +7,7 @@ scalaVersion := "2.12.18"
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.5.4"
 
 libraryDependencies += "org.apache.spark" %% "spark-graphx" % "3.5.4"
+
+libraryDependencies += "org.apache.kafka" %% "kafka-streams-scala" % "3.8.1"
+
+dependencyOverrides += "com.github.luben" % "zstd-jni" % "1.5.5-4" 
--- simulator/src/main/scala/SimulatorApp.scala
+++ simulator/src/main/scala/SimulatorApp.scala
@@ -4,6 +4,9 @@ import org.apache.spark.graphx.{EdgeContext, EdgeDirection, Edge, EdgeTriplet, G
 import org.apache.spark.rdd.RDD
 import org.apache.spark.graphx.util.GraphGenerators
 import com.rug.ants.LangtonAntModel.{Ant, Cell, Direction}
+import java.util.Properties
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord}
+import org.apache.kafka.common.serialization.StringSerializer
 
 object SimulatorApp {
   def main(args: Array[String]): Unit = {
@@ -53,6 +56,7 @@ object SimulatorApp {
       } else {
         new Cell(!cell.colour, Set())
       }
+    
 
     for (i <- 1 to 20) {
       val messages = graph.aggregateMessages[Set[Ant]](msgAnts, mergeAnts).cache()
@@ -62,6 +66,15 @@ object SimulatorApp {
       graph = graph.joinVertices(messages)(handleIncomingAnts).cache()
     }
     printPrettyGrid(graph, gridSize)
+
+    val props = new java.util.Properties()
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "broker:9092")
+    // props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer].getName)
+    // props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer].getName)
+    val producer = new KafkaProducer[String, String](props)
+    val record = new ProducerRecord[String, String]("test-topic", "key", "value")
+    producer.send(record)
+    producer.close()
 
     // while(true){
     //    scala.io.StdIn.readLine() // Hack for keeping spark open
