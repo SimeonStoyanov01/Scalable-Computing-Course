db025c80ee464b9bc61929f745f1ce8d4caaa245
Robin <r.s.b.sachsenweger.ballantyne@student.rug.nl>
2025-03-16 20:28:05 +0000

    change

--- charts/kafka/start-kafka.sh
+++ charts/kafka/start-kafka.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 SCRIPT_DIR=$(dirname "$0")
-helm upgrade --install ants oci://registry-1.docker.io/bitnamicharts/kafka -f $SCRIPT_DIR/values.yaml
+helm upgrade --install kafka oci://registry-1.docker.io/bitnamicharts/kafka -f $SCRIPT_DIR/values.yaml
