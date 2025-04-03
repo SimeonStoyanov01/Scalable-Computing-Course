f642e51087de6b4ff64c847dc06ea7c3203f5c87
Robin <r.s.b.sachsenweger.ballantyne@student.rug.nl>
2025-04-03 13:21:27 +0000

    changed backed replicaSets to 1 and default numsteps to 10

--- simulator/src/main/scala/SimulatorApp.scala
+++ simulator/src/main/scala/SimulatorApp.scala
@@ -89,7 +89,7 @@ object SimulatorApp {
     val gridRowSize = jobRequest.gridRows
     val gridColSize = jobRequest.gridCols
     val numAnts = jobRequest.ants
-    val numSteps = jobRequest.numSteps.getOrElse(10000)
+    val numSteps = jobRequest.numSteps.getOrElse(10)
     val checkpointInterval = jobRequest.checkpointInterval.getOrElse(25)
 
     val checkpointDir = "s3a://checkpoints/"
